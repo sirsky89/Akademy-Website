551388d066f3cadf374549905fb9154d7276e8db
sky <helloheroemail@gmail.com>
2022-11-13 11:09:48 +0800

    first commit

---
+++ components/Layout.js
@@ -0,0 +1,121 @@
+import { signOut, useSession } from 'next-auth/react';
+import Head from 'next/head';
+import Link from 'next/link';
+import Cookies from 'js-cookie';
+import React, { useContext, useEffect, useState } from 'react';
+import { ToastContainer } from 'react-toastify';
+import { Menu } from '@headlessui/react';
+import 'react-toastify/dist/ReactToastify.css';
+import { Store } from '../utils/Store';
+import DropdownLink from './DropdownLink';
+import Image from 'next/image';
+
+export default function Layout({ title, children }) {
+  const { status, data: session } = useSession();
+
+  const { state, dispatch } = useContext(Store);
+  const { cart } = state;
+  const [cartItemsCount, setCartItemsCount] = useState(0);
+  useEffect(() => {
+    setCartItemsCount(cart.cartItems.reduce((a, c) => a + c.quantity, 0));
+  }, [cart.cartItems]);
+
+  const logoutClickHandler = () => {
+    Cookies.remove('cart');
+    dispatch({ type: 'CART_RESET' });
+    signOut({ callbackUrl: '/login' });
+  };
+  return (
+    <>
+      <Head>
+        <title>{title ? title + ' - Akademy' : 'Amazona'}</title>
+        <meta name="description" content="Ecommerce Website" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <ToastContainer position="bottom-center" limit={1} />
+
+      <div className="flex min-h-screen flex-col justify-between bg-rose-400 font-body">
+        <header>
+          <nav className="flex h-16 items-center px-4 justify-between shadow-md">
+            <div className="flex items-center">
+              <Image
+                src="/images/akademy.png"
+                alt="akademy-logo"
+                width={100}
+                height={100}
+              />
+              <Link href="/">
+                <a className="text-lg font-bold uppercase">Akademy</a>
+              </Link>
+            </div>
+            <div>
+              <Link href="/cart">
+                <a className="p-2 uppercase font-bold mr-2">
+                  Cart
+                  {cartItemsCount > 0 && (
+                    <span className="ml-1 rounded-full bg-red-600 px-2 py-1 text-xs font-bold text-white">
+                      {cartItemsCount}
+                    </span>
+                  )}
+                </a>
+              </Link>
+
+              {status === 'loading' ? (
+                'Loading'
+              ) : session?.user ? (
+                <Menu as="div" className="relative inline-block mr-2">
+                  <Menu.Button className=" text-white hover:text-rose-600 uppercase font-bold">
+                    {session.user.name}
+                  </Menu.Button>
+                  <Menu.Items className="absolute right-0 w-56 origin-top-right bg-rose-300  shadow-lg">
+                    <Menu.Item>
+                      <DropdownLink className="dropdown-link" href="/profile">
+                        Profile
+                      </DropdownLink>
+                    </Menu.Item>
+                    <Menu.Item>
+                      <DropdownLink
+                        className="dropdown-link"
+                        href="/order-history"
+                      >
+                        Order History
+                      </DropdownLink>
+                    </Menu.Item>
+                    {session.user.isAdmin && (
+                      <Menu.Item>
+                        <DropdownLink
+                          className="dropdown-link"
+                          href="/admin/dashboard"
+                        >
+                          Admin Dashboard
+                        </DropdownLink>
+                      </Menu.Item>
+                    )}
+                    <Menu.Item>
+                      <a
+                        className="dropdown-link"
+                        href="#"
+                        onClick={logoutClickHandler}
+                      >
+                        Logout
+                      </a>
+                    </Menu.Item>
+                  </Menu.Items>
+                </Menu>
+              ) : (
+                <Link href="/login">
+                  <a className="p-2">Login</a>
+                </Link>
+              )}
+            </div>
+          </nav>
+        </header>
+        <main className="container m-auto mt-4 px-4">{children}</main>
+        <footer className="flex h-10 justify-center items-center shadow-inner">
+          <p>Copyright © 2022 Akademy</p>
+        </footer>
+      </div>
+    </>
+  );
+}
